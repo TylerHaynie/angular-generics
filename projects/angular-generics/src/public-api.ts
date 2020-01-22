a829c987011185edea062b2180ee1117774367a7
Tyler Haynie <tyler@code-t.tech>
2020-01-22 15:39:15 -0600

    control updates

--- projects/angular-generics/src/public-api.ts
+++ projects/angular-generics/src/public-api.ts
@@ -4,9 +4,9 @@ export * from './lib/containers/content/content.module';
 
 // Alert
 export * from './lib/alert/alert.component';
+export * from './lib/alert/alert.module';
 export * from './lib/alert/alert.service';
 export * from './lib/alert/models/generic-alert';
-export * from './lib/alert/alert.module';
 
 // Modal
 export * from './lib/containers/modal/generic-modal.component';
@@ -20,13 +20,17 @@ export * from './lib/containers/page/page-state';
 export * from './lib/controls/button/button.component';
 export * from './lib/controls/button/button.module';
 
+// Input * internal use only *
+// export * from './lib/controls/input/input.component';
+// export * from './lib/controls/input/input.module';
+
+// Checkbox
+export * from './lib/controls/check/check.component';
+export * from './lib/controls/check/check.module';
+
 // File Upload
 export * from './lib/controls/file-upload/file-upload.component';
 export * from './lib/controls/file-upload/file-upload.module';
-
-// Input
-export * from './lib/controls/input/input.component';
-export * from './lib/controls/input/input.module';
 
 // Range
 export * from './lib/controls/range/range.component';
@@ -42,6 +46,11 @@ export * from './lib/navigation/side-nav/side-nav.component';
 export * from './lib/navigation/top-nav/top-nav.component';
 export * from './lib/navigation/navigation.module';
 
+// DataTable 
+export * from './lib/controls/data-table/table/table-data-source';
+export * from './lib/controls/data-table/table/table.component';
+export * from './lib/controls/data-table/table/table.module';
+
 // Table Filter
 export * from './lib/controls/data-table/filter/table-filter.component';
 export * from './lib/controls/data-table/filter/table-filter.module';
@@ -49,11 +58,6 @@ export * from './lib/controls/data-table/filter/table-filter.module';
 // Table Pager
 export * from './lib/controls/data-table/pager/table-pager.component';
 export * from './lib/controls/data-table/pager/table-pager.module';
-
-// Table 
-export * from './lib/controls/data-table/table/table-data-source';
-export * from './lib/controls/data-table/table/table.component';
-export * from './lib/controls/data-table/table/table.module';
 
 // DataTable Models
 export * from './lib/controls/data-table/models/column-filter';

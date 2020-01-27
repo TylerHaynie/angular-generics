b30090726c25edd7902f302aa5b6596c42464c41
TylerHaynie <tyler@code-t.tech>
2020-01-27 16:47:42 -0600

    refactor to use base

--- projects/angular-generics/src/lib/controls/check/check.component.ts
+++ projects/angular-generics/src/lib/controls/check/check.component.ts
@@ -1,42 +1,16 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
-import { MakeProvider, AbstractValueAccessor } from '../generic-control/abstract-value-accessor';
-import { ValueTypeHelpers } from '../generic-control/value-type-helpers';
+import { GenericInput } from '../generic-control/generic-input';
 
 @Component({
   selector: 'ag-check',
   templateUrl: './check.component.html',
   styleUrls: ['./check.component.css'],
-  providers: [MakeProvider(CheckComponent)],
   encapsulation: ViewEncapsulation.None
 })
-export class CheckComponent extends AbstractValueAccessor {
-  // General
-  @Input() name: string;
-  @Input() disabled: boolean = false;
-  @Input() required: boolean = false;
-  @Input() type: string = 'text';
-  @Input() subtype: string = 'default';
-
-  @Input() placeholder: string = '';
-  @Input() width: number | string = 'inherit';
-  @Input() height: number | string = 'inherit';
-
-  // Label
-  @Input() showLabel: boolean = true;
-  @Input() floatLabel: boolean = true;
-  @Input() placeLabel: string = 'bottom start';
-
-  // Text Area
-  @Input() textRows: number = 5;
-  @Input() textColumns: number = 0;
-
-  private _helpers: ValueTypeHelpers = new ValueTypeHelpers();
+export class CheckComponent extends GenericInput {
+  @Input() type: string;
 
   constructor() {
     super();
-  }
-
-  isNumber(value: string | number): boolean {
-    return this._helpers.isNumber(value);
   }
 }

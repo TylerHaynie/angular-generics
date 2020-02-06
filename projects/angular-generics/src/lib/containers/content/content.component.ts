553d44c8994f2b30dbd6164742264d6646c11df0
TylerHaynie <tyler@code-t.tech>
2020-02-05 19:45:10 -0600

    content properties added.

--- projects/angular-generics/src/lib/containers/content/content.component.ts
+++ projects/angular-generics/src/lib/containers/content/content.component.ts
@@ -15,10 +15,17 @@ export class ContentComponent {
   @Input() place: string;
 
   @Input() flow: string;
-  @Input() rows: number | string = 1;
-  @Input() cols: number | string = 1;
+  @Input() rows: number | string;
+  @Input() cols: number | string;
+
+  @Input() forceUp: number | string;
+  @Input() forceDown: number | string;
+  @Input() forceLeft: number | string;
+  @Input() forceRight: number | string;
 
   private typeHelper: ValueTypeHelpers = new ValueTypeHelpers();
+
+  constructor() { }
 
   isNumber(value: string | number): boolean {
     return this.typeHelper.isNumber(value);
